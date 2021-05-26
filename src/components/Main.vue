c58fb58cfa880784ff8617ca250c7b35f7a788be
bigoroberto <bigoroberto@gmail.com>
2021-05-26 18:50:57 +0200

    milestone1 completato

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,21 @@
 <template>
-<main>
-  <div>
-      {{listaFilm}}
+<main> 
+  <div v-for="(film, index) in listaFilm"
+      :key="index">
+      <ul>
+        <li>
+        <h3>Titolo del film:  {{film.title}}</h3>
+        </li>
+        <li>
+        <h3>Titolo originale:  {{film.original_title}}</h3>
+        </li>
+        <li>
+        <h3>Lingua originale:  {{film.original_language}}</h3>
+        </li>
+        <li>
+        <h3>Voto:  {{film.vote_average}}</h3>
+        </li>
+      </ul>
   </div>
 </main>
 </template>
