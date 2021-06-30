78fede514edc273962037198f1f8288860d4bdab
bigoroberto <bigoroberto@gmail.com>
2021-06-30 13:42:12 +0200

    correzione

--- src/App.vue
+++ src/App.vue
@@ -1,152 +1,108 @@
 <template>
   <div id="app">
 
-    <!-- catturo l'evento search lanciato in header -->
-    <Header 
-      @search="search" 
-    />
+    <!-- all'eveto $emit startSearch invoco la funzione startSerach -->
+    <Haed @startSearch="startSearch" />
 
-    <!-- aggiungo un contenitore con background e descrizione pagina che visualizzo solo all'accesso -->
-    <!-- utilizzo una condizione -->
-    <div 
-      v-if="welcome"
-      class="position-relative"
-    >
-      <div class="welcome"></div>
-      <div class="mc_bool position-absolute">
-        <h1>boolflix</h1>
-        <h3>Film, serie TV e tanto altro.</h3>
-      </div>
-    </div>
+    <!-- in caso di assenza di risultati visualizzo questo h1 -->
+    <h1 v-if="results.movie.length === 0 && results.tv.length === 0">Nessun risultato trovato</h1>
 
-    <!-- se show è true mostro l'h1 e il relativo messaggio -->
-    <div class="d-flex justify-content-center align-items-center mc_msg">
-      <h1 
-        v-if='show'
-        class="text-center"
-      >
-       nessun risultato trovato
-      </h1>
-      <!-- <h1 v-if='results.movie.length === 0 && results.movie.length === 0'>nessun risultato</h1> -->
-    </div>
+    <!-- ho 2 componenti Main per movie e tv e li visualizzo solo se coi sono elementi nell'array -->
+    <!-- passo 2 pros: tipo di dato (movie o tv) l'elenco dei risultati -->
+    <Main v-if="results.movie.length > 0"  type='movie' :list="results.movie" />
+    <Main v-if="results.tv.length > 0" type='tv' :list="results.tv"/>
 
-    <!-- vedo Main solo se l'array movie contiene qualcosa -->
-    <!-- binding di results.movie -->
-    <Main 
-      v-if="results.movie.length > 0" 
-      type="movie" 
-      :list="results.movie" 
-    />
-
-    <!-- vedo Main solo se l'array tv contiene qualcosa -->
-    <!-- binding di results.tv -->
-    <Main 
-      v-if="results.tv.length > 0" 
-      type="tv" 
-      :list="results.tv" 
-    />
   </div>
 </template>
 
 <script>
-// importo in app...
-import axios from "axios";
-import Header from "./components/Header.vue";
-import Main from "./components/Main.vue";
+import Haed from './components/Haed';
+import Main from './components/Main';
+import axios from 'axios';
 
 export default {
-  name: "App",
-  // dichiaro i componenti esportati in APP
+  name: 'App',
   components: {
-    Header,
-    Main,
+    Haed,
+    Main
   },
-  data() {
-    return {
-      apiURL: "https://api.themoviedb.org/3/search/",
-      apiKey: "8aba2f0fc3e09fb1de7523aaaf3513bc",
-      query: "",
-      welcome: true, // se true mostro un elemento di default con background di 'benvenuto'. Dopo la chiamata API diventa false
-      show: false, // utilizzo la variabile show per mostrare o no il messaggio 'nessun rislutato trovato'
-      results: {  // oggetto che contiene due proprietà/array. Servono nel main per smistare i risultati in base al tipo
-        movie: [],
-        tv: [],
-      },
-    };
+  data(){
+    return{
+      apiUrl: 'https://api.themoviedb.org/3/search/',
+      apiKey: '1b3eb153fce73eb6b953f7d515b2dc1d',
+      // in questo oggetto memorizzo le 2 ricerche
+      results:{
+        'movie':[],
+        'tv':[]
+      }
+    }
   },
-  methods: {
-    // funzione che richiama la funzione searching sia in movie che in tv
-    // doppia chiamata API
-    // obj è il secondo parametro passato nell'$emit (in header)
-    search(obj) {
-      if (obj.type === "all") {
-        this.searching(obj.text, "movie");
-        this.searching(obj.text, "tv");
+  methods:{
+
+    // lancio la ricerca
+    startSearch(obj){
+      // resette le ricerche passate
+      this.resetResults();
+      // se "CERCA TUTTO" faccio 2 chiamate
+      if(obj.type === 'all'){
+        this.getAPI(obj.text, 'movie');
+        this.getAPI(obj.text, 'tv');
+      }else{
+        this.getAPI(obj.text, obj.type);
       }
     },
-    // funzione che permette di eseguire la chiamata API avviando la ricerca di query(del testo inserito dall'utente)
-    searching(query, type) {
-      // se query non è un campo vuoto...
-      if (query != "") {
-        this.query = query;
-        this.show = false;
-        // ... effetto la chiamata API
-        axios
-          .get(this.apiURL + type, {
-            params: {
+
+    resetResults(){
+      this.results.movie = [];
+      this.results.tv = [];
+    },
+
+    // funzione per la chiamata axios
+    getAPI(query, type){
+      
+      // effettuo la chiamata solo se c'è un testo da cercare
+      if(query !== ''){
+        axios.get(this.apiUrl+type,{
+            params:{
               api_key: this.apiKey,
-              query: this.query,
-              language: "it-IT",
-            },
-          })
-          .then((resp) => {
-            // ricevo la risposta
-            // visualizzo la risposta nel relativo array di oggetti (movie o tv)
-            this.results[type] = resp.data.results;
-            console.log(this.results);
-            // se non è stato trovato nessun risultato trasformo show in true.
-            if(this.results.movie.length === 0 && this.results.tv.length === 0){
-              this.show = true // v-if='show = true' mostra il messaggio 'Nessun risultato trovato'
+              query: query,
+              language: 'it-IT'
             }
-            this.welcome = false // non mostro più l'elemento di 'benvenuto'
           })
-          .catch((error) => {
-            console.log(error);
-          });
+          .then(res => {
+            // in base al tipo di ricerca slavo il dato nell'array delloggetto results
+            this.results[type] = res.data.results;
+            console.log('MOVIE',this.results.movie);
+            console.log('TV', this.results.tv);
+          })
+          .catch(err => {
+            console.log(err);
+          })
       }
-    },
+    }
+
   },
-};
+  created(){
+    
+      let type = 'movie'
+      axios.get('https://api.themoviedb.org/3/movie/popular',{
+          params:{
+            api_key: this.apiKey,
+            language: 'it-IT'
+          }
+        })
+        .then(res => {
+           this.results[type] = res.data.results;
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    
+  }
+}
 </script>
 
 <style lang="scss">
-// importo il foglio di stile general.scss
-@import "./assets/style/general";
-.welcome {
-  width: 100%;
-  height: 90vh;
-  background-image: url('./assets/img/boolflix.png');
-  filter: opacity(0.2);
-}
-.mc_bool {
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  h1 {
-    font-size: 3em;
-    color: $color-btn;
-    text-transform: uppercase;
-    text-shadow: 0px 3px 2px rgba(12, 10, 10, 0.767);
-  }
-  h3 {
-    font-size: 3em;
-  }
-}
-.mc_msg {
-  height: 80vh;
-  position: absolute;
-  left: 50%;  
-  transform: translate(-50%);
-}
+@import './assets/styles/general';
+
 </style>
---
+++ src/components/Haed.vue
@@ -0,0 +1,69 @@
+<template>
+  <header>
+
+    <div class="d-flex pt-3 justify-content-between px-3">
+      <div>
+        <img src="https://fontmeme.com/permalink/210528/323aa9a77404d38b71457254b1d10499.png" alt="Boolflix">
+      </div>
+      <div class="form d-flex px-3">
+        <div class="flex-grow-1">
+          <input 
+          v-model.trim="textToSearch"
+          @keyup.enter="sendEmit()"
+          placeholder="Cerca..."
+          class="form-control"
+          type="text" />
+
+        </div>
+        <div>
+          <!-- all'$emit invio un oggetto con 2 parametri, il testo da cercare e il tipo di ricerca da fare -->
+          <select class="form-select mx-3 "
+              v-model="typeSelected"
+              @change="sendEmit()">
+            <option value="all">All</option>
+            <option value="movie">Film</option>
+            <option value="tv">Tv</option>
+          </select>
+        </div>
+  
+          
+ 
+
+      </div>
+
+    </div>
+    
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Haed',
+  data(){
+    return{
+      textToSearch: '',
+      typeSelected: 'all'
+    }
+  },
+  methods:{
+    sendEmit(){
+      this.$emit('startSearch',{text:this.textToSearch, type:this.typeSelected})
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+  height: 70px;
+
+  background-image:linear-gradient(#000 ,#383838) 
+}
+img{
+  width: 100px;
+}
+.form{
+  flex-basis: 50%;
+}
+
+</style>
